eb7b88834b0a30ac23325069ff22b1cf6ec32cdb
yaoyang <yaoyang518@126.com>
2019-03-23 10:36:07 +0800

    登录接口

--- src/main/java/com/yaoyang/bser/controller/UserController.java
+++ src/main/java/com/yaoyang/bser/controller/UserController.java
@@ -35,19 +35,17 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/login")
-    @ApiOperation(value = "登陆-前端")
+    @ApiOperation(value = "登陆-前端",notes = "level:1是超管，2是市管，3是网管")
     public ApiResult login(HttpServletRequest request, @RequestParam(value = "loginName") String loginName,
                            @RequestParam(value = "password") String password) {
         if (!CommonUtil.isRightPhone(loginName)) {
             return ApiResultBuilder.buildFailedResult(ResponseCode.MOBILE_ILLEGAL);
         }
-        User user = userService.findByMobileAndPassword(loginName, Md5Util.MD5(password));
+        User user = userService.findByMobileAndPassword(loginName, password);
         if (user == null) {
             return ApiResultBuilder.buildFailedResult(ResponseCode.PASSWORD_ERROR, "账号或密码错误", null);
         }
-        String token = Jwts.builder().setSubject(loginName)
-                .setExpiration(new DateTime().plusHours(8).toDate()).compact();
-        return ApiResultBuilder.buildSuccessResult(ResponseCode.OPT_SUCCESS, token);
+        return ApiResultBuilder.buildSuccessResult(ResponseCode.OPT_SUCCESS, user);
     }
 
 }
--- src/main/java/com/yaoyang/bser/entity/User.java
+++ src/main/java/com/yaoyang/bser/entity/User.java
@@ -1,17 +1,31 @@
 package com.yaoyang.bser.entity;
 
-import com.yaoyang.bser.base.BaseEntity;
-
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
 
 @Entity
-public class User extends BaseEntity {
+@Table(name = "sys_user")
+public class User implements Serializable {
 
+    @Id
+    private Long userid;
 
+    private Date createTime = new Date();
+
+    private Date updateTime = new Date();
     private String mobile;
     private String username;
-    private String nickName;
+    private String realname;
     private String password;
+    private String mail;
+    private Long cid;
+    private Long dotid;
+    private String areaadree;
+    private int level;
+    private int userstaus;
 
     public String getMobile() {
         return mobile;
@@ -37,13 +51,83 @@ public class User extends BaseEntity {
         this.password = password;
     }
 
-
-    public String getNickName() {
-        return nickName;
+    public Long getUserid() {
+        return userid;
     }
 
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
+    public void setUserid(Long userid) {
+        this.userid = userid;
     }
 
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Long getCid() {
+        return cid;
+    }
+
+    public void setCid(Long cid) {
+        this.cid = cid;
+    }
+
+    public Long getDotid() {
+        return dotid;
+    }
+
+    public void setDotid(Long dotid) {
+        this.dotid = dotid;
+    }
+
+    public String getAreaadree() {
+        return areaadree;
+    }
+
+    public void setAreaadree(String areaadree) {
+        this.areaadree = areaadree;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getUserstaus() {
+        return userstaus;
+    }
+
+    public void setUserstaus(int userstaus) {
+        this.userstaus = userstaus;
+    }
 }
